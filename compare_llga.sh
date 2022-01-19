55ba37406bf5a77939cb056abc7c4baaf003b58f
sanchitintel <sanchit.jain@intel.com>
2022-01-19 00:16:51 -0800

    Enable benchmarking of 7 more models

--- compare_llga.sh
+++ compare_llga.sh
@@ -5,7 +5,14 @@ test_bench.py::TestBenchNetwork::test_eval[resnet50-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[densenet121-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[squeezenet1_1-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[vgg16-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[alexnet-cpu-jit]"
+test_bench.py::TestBenchNetwork::test_eval[alexnet-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[mobilenet_v2-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[mnasnet1_0-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_resnet-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[resnet18-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[shufflenet_v2_x1_0-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[mobilenet_v3_large-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_regnet-cpu-jit]"
 
 pytest --ignore_machine_config $models --cpu_only --benchmark-json nollga.json
 pytest --ignore_machine_config $models --fuser llga --cpu_only --benchmark-json llga.json
