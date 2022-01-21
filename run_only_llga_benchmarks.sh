c375c21824e76db744dfa001e6dbac8597d9ffc4
sanchit.jain <sanchit.jain@intel.com>
2022-01-20 19:10:56 -0800

    Add script to run only llga supported vision models

---
+++ run_only_llga_benchmarks.sh
@@ -0,0 +1,21 @@
+export OMP_NUM_THREADS=1
+
+models="test_bench.py::TestBenchNetwork::test_eval[resnext50_32x4d-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[resnet50-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[densenet121-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[squeezenet1_1-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[vgg16-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[alexnet-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[mobilenet_v2-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[mnasnet1_0-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_resnest-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[resnet18-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[shufflenet_v2_x1_0-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[mobilenet_v3_large-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_regnet-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_vision_transformer-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_efficientnet-cpu-jit] \
+test_bench.py::TestBenchNetwork::test_eval[timm_vovnet-cpu-jit]"
+
+pytest --ignore_machine_config $models --cpu_only --fuser llga
+
