feef4870162dd1e5fcbc7d5116df89b89a63ee44
sanchit.jain <sanchit.jain@intel.com>
2022-04-06 02:02:52 -0700

    Enable more models' benchmarking for BF16

--- compare_llga.sh
+++ compare_llga.sh
@@ -1,22 +1,14 @@
-export OMP_NUM_THREADS=1
-
 models="test_bench.py::TestBenchNetwork::test_eval[resnext50_32x4d-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[resnet50-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[densenet121-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[squeezenet1_1-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[vgg16-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[alexnet-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[mobilenet_v2-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[mnasnet1_0-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[timm_resnest-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[resnet18-cpu-jit] \
 test_bench.py::TestBenchNetwork::test_eval[shufflenet_v2_x1_0-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[mobilenet_v3_large-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[timm_regnet-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[timm_vision_transformer-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[timm_efficientnet-cpu-jit] \
-test_bench.py::TestBenchNetwork::test_eval[timm_vovnet-cpu-jit]"
+test_bench.py::TestBenchNetwork::test_eval[mobilenet_v3_large-cpu-jit]"
 
-pytest --ignore_machine_config $models --cpu_only --benchmark-json nollga.json
-pytest --ignore_machine_config $models --fuser llga --cpu_only --benchmark-json llga.json
-python compare.py nollga.json llga.json
+pytest --ignore_machine_config $models --fuser llga --dtype fp32 --cpu_only --benchmark-json llga_fp32.json
+pytest --ignore_machine_config $models --fuser llga --dtype bf16 --cpu_only --benchmark-json llga_bf16.json
+python compare.py llga_fp32.json llga_bf16.json
--- torchbenchmark/models/resnext50_32x4d/__init__.py
+++ torchbenchmark/models/resnext50_32x4d/__init__.py
@@ -4,6 +4,6 @@ from torchbenchmark.tasks import COMPUTER_VISION
 class Model(TorchVisionModel):
     task = COMPUTER_VISION.CLASSIFICATION
 
-    def __init__(self, device=None, jit=False, fuser="", train_bs=8, eval_bs=8, extra_args=[]):
-        super().__init__(model_name="resnext50_32x4d", device=device, jit=jit, fuser=fuser,
+    def __init__(self, device=None, jit=False, fuser="", dtype="fp32", train_bs=8, eval_bs=8, extra_args=[]):
+        super().__init__(model_name="resnext50_32x4d", device=device, jit=jit, fuser=fuser, dtype=dtype,
                          train_bs=train_bs, eval_bs=eval_bs, extra_args=extra_args)
